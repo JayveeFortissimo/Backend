89808f4c2626670ac33f2861b1e4a3c1879893d8
Derssmnemashima <maniquizjayvee25@gmail.com>
2024-11-09 04:50:53 +0800

    New

--- Model/Database.js
+++ Model/Database.js
@@ -2,13 +2,21 @@ import mysql from 'mysql2';
 import env from 'dotenv';
 env.config();
 const db = mysql.createConnection({
-    host:process.env.HOST,
-    user:process.env.USER,
-    password:'',
-    database:process.env.DATABASE
+    host:process.env.DB_HOST,
+    user:process.env.D_BUSER,
+    password:process.env.DB_USERPASSWORD,
+    database:process.env.DB_DATABASE
 });
 
-db?console.log("Connected"):console.log("not connected");
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        throw err;
+    } else {
+        console.log('Connected to database!');
+    }
+});
 
 
 export default db;
