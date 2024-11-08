d7fa195d5b73b13df90f329b36679ed5b397acae
Derssmnemashima <maniquizjayvee25@gmail.com>
2024-11-09 05:36:35 +0800

    Y

--- Model/Database.js
+++ Model/Database.js
@@ -2,11 +2,20 @@ import mysql from 'mysql2';
 import env from 'dotenv';
 
 env.config();
+
+
+console.log("Host:", process.env.DB_HOST || "undefined");
+console.log("User:", process.env.DB_USER || "undefined");
+console.log("Password:", process.env.DB_USERPASSWORD || "undefined");
+console.log("Database:", process.env.DB_DATABASE || "undefined");
+
+
+
 const db = mysql.createConnection({
-    host:process.env.DB_HOST.toString(),
-    user:process.env.DB_USER.toString(),
-    password:process.env.DB_USERPASSWORD.toString(),
-    database:process.env.DB_DATABASE.toString()
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_USERPASSWORD,
+    database:process.env.DB_DATABASE
 });
 
 //TRYassdasd
