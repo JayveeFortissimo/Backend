2ffb0d29be8b53e580e6efd1061d96aba3ad56ae
Derssmnemashima <maniquizjayvee25@gmail.com>
2024-11-09 23:02:41 +0800

    Domain

--- Main.js
+++ Main.js
@@ -28,6 +28,16 @@ app.use(cors({
 }));
 
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,token');
+  next();
+}
+
+app.use(allowCrossDomain);
+
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
