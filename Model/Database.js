e81f2bada0b1311ea4125c0ee18c3bce0805dd4b
Derssmnemashima <maniquizjayvee25@gmail.com>
2024-11-10 00:17:02 +0800

    Hey

--- Model/Database.js
+++ Model/Database.js
@@ -6,11 +6,15 @@ env.config();
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_USERPASSWORD,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 });
 
 console.log("Myport:", process.env.PORT)
+console.log("DB_HOST:", process.env.DB_HOST);   // Should print 'srv1415.hstgr.io'
+console.log("DB_USER:", process.env.DB_USER);   // Should print 'u120939471_cristobal'
+console.log("DB_DATABASE:", process.env.DB_DATABASE);   // Should print 'u120939471_cristobal'
+
 
 db.connect((err) => {
     if (err) {
