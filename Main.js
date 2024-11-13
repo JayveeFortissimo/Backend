7acc65e5045b1e1b427c9831dca226b57fd34523
Derssmnemashima <maniquizjayvee25@gmail.com>
2024-11-14 06:48:09 +0800

    RRS

--- Main.js
+++ Main.js
@@ -52,7 +52,10 @@ io.on('connection', (socket) => {
 });
 
 // For accessing images added by admin
-app.use('/uploads', express.static('./uploads'));
+//app.use('/uploads', express.static('./uploads'));
+ 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 app.use((req, res, next) => {
   req.io = io;
